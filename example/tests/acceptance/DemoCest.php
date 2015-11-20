34c6bddd4f86b24f11c5169395796fd8b7ccd988
dino.korah <dino@redmatter.com>
2015-11-20 12:15:40 +0000

    Added more notes in the Demo and updated README

--- example/tests/acceptance/DemoCest.php
+++ example/tests/acceptance/DemoCest.php
@@ -6,6 +6,12 @@ use \AcceptanceTester;
 
 class DemoCest
 {
+    /**
+     * IMPORTANT NOTE: This is just a demo and it is not the most common use-case for the CtrlC module.
+     * The benefits of this module can be enjoyed just by adding it to the list of enabled modules, in the config.
+     *
+     * @param AcceptanceTester $I
+     */
     public function demo(AcceptanceTester $I)
     {
         $seconds = 60;
